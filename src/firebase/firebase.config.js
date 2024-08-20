3a1fe7b67d118c2838c322a72c3387a17313fc3a
Lalon Sheikh <you@example.com>
2024-08-20 06:07:37 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6FXyCZzXOM2vmtUe7aVm6OLIeokSW2ks",
+  authDomain: "solosphere-24024.firebaseapp.com",
+  projectId: "solosphere-24024",
+  storageBucket: "solosphere-24024.appspot.com",
+  messagingSenderId: "37453210651",
+  appId: "1:37453210651:web:8b355aa9f6ece837cc44be"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
